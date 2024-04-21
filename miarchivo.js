289cc955365c1ff593175a7cc680beb67bb92ad0
RafaelCTobar <rafaelctobar569@gmail.com>
2024-04-21 15:00:24 -0500

    fix

--- miarchivo.js
+++ miarchivo.js
@@ -1,5 +1,11 @@
 import {chromium} from "playwright";
 
+async function wait(timer){
+    return new Promise((resolve)=>{
+        setTimeout(resolve, timer);
+    })
+}
+
 async function openWebPage(numRuc) {
     console.log("Entrando al scrap")
     let browser = null;
@@ -14,60 +20,51 @@ async function openWebPage(numRuc) {
         const page = await browser.newPage({
             userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537' 
         });
-        console.log("despues de la page")
+        console.log("despues de crear una nueva page")
         
         await page.goto('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp', {timeout: 60000});
         console.log("llego hasta la conexion con el link");
 
         await page.click("#txtRuc"),
         await page.fill("#txtRuc", numRuc),
-
         
         await page.click("#btnAceptar"),
-        // await page.waitForLoadState('load');
         await page.waitForURL("https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias")
         console.log("Espero a la url");
-
-        await page.waitForSelector(".list-group", {state: 'visible', timeout: 30000});
-  
+        await wait(3000);
+        console.log("Esperó los 3 segundos.")
 
         const result = await page.evaluate(() => {
+            const childrens = document.querySelector(".list-group").children;
 
-            const elemento = [...document.querySelector(".list-group").children];
-            
-            const numeroRucName =  elemento[0].children[0].children[1].children[0].innerText.trim(); // el primer corchete decide cual salir
-            const tipoContribuyente =  elemento[1].children[0].children[1].children[0].innerText.trim();
-            const nombreComercial = elemento[2].children[0].children[1].children[0].innerHTML.trim(); 
-            const fechaInscripcion = elemento[3].children[0].children[1].children[0].innerText.trim();
-            const estadoContribuyente = elemento[4].children[0].children[1].children[0].innerText.trim(); 
-            const condicionContribuyente = elemento[5].children[0].children[1].children[0].innerText.trim(); 
-            const domicilioFiscal = elemento[6].children[0].children[1].children[0].innerText.trim();
-            const sistemaEmisionComprobante = elemento[7].children[0].children[1].children[0].innerText.trim();
-            const actividadComercio = elemento[7].children[0].children[3].children[0].innerText.trim();
-            const sistemaContabilidad = elemento[8].children[0].children[1].children[0].innerText.trim();
-        
-            const emisorElectronicoDesde = elemento[12].children[0].children[1].children[0].innerText.trim();
-            const comprobanteElectronico = elemento[13].children[0].children[1].children[0].innerText.trim();
-            const afiliadoAlPleDesde = elemento[14].children[0].children[1].children[0].innerText.trim();
-            
-            
-            return {
-                numeroRucName: numeroRucName,
-                tipoContribuyente: tipoContribuyente,
-                nombreComercial: nombreComercial,
-                fechaInscripcion: fechaInscripcion,
-                estadoContribuyente: estadoContribuyente,
-                condicionContribuyente: condicionContribuyente,
-                domicilioFiscal: domicilioFiscal,
-                sistemaEmisionComprobante: sistemaEmisionComprobante,
-                actividadComercio: actividadComercio,
-                sistemaContabilidad:sistemaContabilidad,
-                emisorElectronicoDesde:emisorElectronicoDesde,
-                comprobanteElectronico:comprobanteElectronico,
-                afiliadoAlPleDesde: afiliadoAlPleDesde,
-            
+            const obtenerTexto = (childrens, childIndex) => {
+                return childrens.children[0].children[childIndex].children[0].innerText.trim();
             }
-        
+
+            const campos = [
+                { nombre: 'numeroRucName', indice: 1, elementoIndice: 0 },
+                { nombre: 'tipoContribuyente', indice: 1, elementoIndice: 1 },
+                { nombre: 'nombreComercial', indice: 1, elementoIndice: 2 },
+                { nombre: 'fechaInscripcion', indice: 1, elementoIndice: 3 },
+                { nombre: 'estadoContribuyente', indice: 1, elementoIndice: 4 },
+                { nombre: 'condicionContribuyente', indice: 1, elementoIndice: 5 },
+                { nombre: 'domicilioFiscal', indice: 1, elementoIndice: 6 },
+                { nombre: 'sistemaEmisionComprobante', indice: 1, elementoIndice: 7 },
+                { nombre: 'actividadComercio', indice: 3, elementoIndice: 7 },
+                { nombre: 'sistemaContabilidad', indice: 1, elementoIndice: 8 },
+                { nombre: 'emisorElectronicoDesde', indice: 1, elementoIndice: 12 },
+                { nombre: 'comprobanteElectronico', indice: 1, elementoIndice: 13 },
+                { nombre: 'afiliadoAlPleDesde', indice: 1, elementoIndice: 14 }
+            ];
+
+            const resultado = {};
+
+            campos.forEach((campo) => {
+                resultado[campo.nombre] = obtenerTexto(childrens[campo.elementoIndice], campo.indice);
+            });
+
+            return resultado;
+
         })
 
 
