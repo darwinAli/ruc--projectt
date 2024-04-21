eb9e8f99d60adfeed0a8e07445f5cd200955d761
RafaelCTobar <rafaelctobar569@gmail.com>
2024-04-21 01:05:01 -0500

    chore: change timer

--- miarchivo.js
+++ miarchivo.js
@@ -22,43 +22,43 @@ async function openWebPage(numRuc) {
         await page.click("#txtRuc");
         await page.fill("#txtRuc", numRuc);
         await page.click("#btnAceptar");
-        await page.waitForSelector(".list-group", {timeout: 60000, state: 'attached' });
+        await page.waitForSelector(".list-group", {timeout: 120000, state: 'attached' });
 
         const result = await page.evaluate(() => {
             console.log("Llegamos a evaluate");
             const elemento = document.querySelector(".list-group").children;
-            // const numeroRucName =  elemento[0].children[0].children[1].children[0].innerText.trim(); // el primer corchete decide cual salir
-            // const tipoContribuyente =  elemento[1].children[0].children[1].children[0].innerText.trim();
-            // const nombreComercial = elemento[2].children[0].children[1].children[0].innerHTML.trim(); 
-            // const fechaInscripcion = elemento[3].children[0].children[1].children[0].innerText.trim();
-            // const estadoContribuyente = elemento[4].children[0].children[1].children[0].innerText.trim(); 
-            // const condicionContribuyente = elemento[5].children[0].children[1].children[0].innerText.trim(); 
-            // const domicilioFiscal = elemento[6].children[0].children[1].children[0].innerText.trim();
-            // const sistemaEmisionComprobante = elemento[7].children[0].children[1].children[0].innerText.trim();
-            // const actividadComercio = elemento[7].children[0].children[3].children[0].innerText.trim();
-            // const sistemaContabilidad = elemento[8].children[0].children[1].children[0].innerText.trim();
+            const numeroRucName =  elemento[0].children[0].children[1].children[0].innerText.trim(); // el primer corchete decide cual salir
+            const tipoContribuyente =  elemento[1].children[0].children[1].children[0].innerText.trim();
+            const nombreComercial = elemento[2].children[0].children[1].children[0].innerHTML.trim(); 
+            const fechaInscripcion = elemento[3].children[0].children[1].children[0].innerText.trim();
+            const estadoContribuyente = elemento[4].children[0].children[1].children[0].innerText.trim(); 
+            const condicionContribuyente = elemento[5].children[0].children[1].children[0].innerText.trim(); 
+            const domicilioFiscal = elemento[6].children[0].children[1].children[0].innerText.trim();
+            const sistemaEmisionComprobante = elemento[7].children[0].children[1].children[0].innerText.trim();
+            const actividadComercio = elemento[7].children[0].children[3].children[0].innerText.trim();
+            const sistemaContabilidad = elemento[8].children[0].children[1].children[0].innerText.trim();
         
-            // const emisorElectronicoDesde = elemento[12].children[0].children[1].children[0].innerText.trim();
-            // const comprobanteElectronico = elemento[13].children[0].children[1].children[0].innerText.trim();
-            // const afiliadoAlPleDesde = elemento[14].children[0].children[1].children[0].innerText.trim();
+            const emisorElectronicoDesde = elemento[12].children[0].children[1].children[0].innerText.trim();
+            const comprobanteElectronico = elemento[13].children[0].children[1].children[0].innerText.trim();
+            const afiliadoAlPleDesde = elemento[14].children[0].children[1].children[0].innerText.trim();
             
             
-            // return {
-            //     numeroRucName: numeroRucName,
-            //     tipoContribuyente: tipoContribuyente,
-            //     nombreComercial: nombreComercial,
-            //     fechaInscripcion: fechaInscripcion,
-            //     estadoContribuyente: estadoContribuyente,
-            //     condicionContribuyente: condicionContribuyente,
-            //     domicilioFiscal: domicilioFiscal,
-            //     sistemaEmisionComprobante: sistemaEmisionComprobante,
-            //     actividadComercio: actividadComercio,
-            //     sistemaContabilidad:sistemaContabilidad,
-            //     emisorElectronicoDesde:emisorElectronicoDesde,
-            //     comprobanteElectronico:comprobanteElectronico,
-            //     afiliadoAlPleDesde: afiliadoAlPleDesde,
+            return {
+                numeroRucName: numeroRucName,
+                tipoContribuyente: tipoContribuyente,
+                nombreComercial: nombreComercial,
+                fechaInscripcion: fechaInscripcion,
+                estadoContribuyente: estadoContribuyente,
+                condicionContribuyente: condicionContribuyente,
+                domicilioFiscal: domicilioFiscal,
+                sistemaEmisionComprobante: sistemaEmisionComprobante,
+                actividadComercio: actividadComercio,
+                sistemaContabilidad:sistemaContabilidad,
+                emisorElectronicoDesde:emisorElectronicoDesde,
+                comprobanteElectronico:comprobanteElectronico,
+                afiliadoAlPleDesde: afiliadoAlPleDesde,
             
-            // }
+            }
             return {elemento}
             
         })
