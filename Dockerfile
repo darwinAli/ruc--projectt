b0b69a7b02ee7c8b8fbc6e51aa41d2510daf8f76
RafaelCTobar <rafaelctobar569@gmail.com>
2024-04-20 23:09:13 -0500

    message

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,45 @@
-# Build Environment: Node + Playwright
-FROM node:20
-FROM mcr.microsoft.com/playwright:v1.43.0-jammy
+# Build Environment: Node + Puppeteer
+FROM node:17
+
+# Instalar las dependencias necesarias para Puppeteer
+RUN apt-get update && apt-get install -y \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libfontconfig1 \
+    libgbm1 \
+    libgcc1 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxss1 \
+    libxtst6 \
+    lsb-release \
+    wget \
+    xdg-utils
 
 # Env
 WORKDIR /app
@@ -13,11 +52,9 @@ EXPOSE 3001
 COPY package*.json ./
 COPY miarchivo.js ./
 COPY server.js ./
-COPY testDelete.js ./
 
 # Install Deps
 RUN npm install
-RUN npx playwright install
 
 # Run Node index.js file
 CMD [ "npm", "start" ]
--- miarchivo.js
+++ miarchivo.js
@@ -1,83 +1,30 @@
-import { firefox } from 'playwright';
-
-async function wait(timer){
-    return new Promise((res, rej)=>{
-        setTimeout(()=>{
-            res();
-        }, timer)
-    })
-}
+import puppeteer from 'puppeteer';
 
 async function openWebPage(numRuc) {
     console.log("Entrando al scrap")
     let browser = null;
 
     try {
-        browser = await firefox.launch({
+        browser = await puppeteer.launch({
             headless: true
         });
         console.log("despues del browser")
 
-        const context = await browser.newContext();
-        console.log("despues del context")
-        const page = await context.newPage();
+        const page = await browser.newPage();
         console.log("despues de la page")
 
-    
         await page.goto('https://adevsays.com');
-        console.log("llego hsata la conexion")
-
-            // await page.goto('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp', { timeout: 60000 });
-
+        console.log("llego hasta la conexion");
         const result = await page.evaluate(()=>{
             const el = document.querySelector("h1");
             return {
                 [el.tagName]: el.textContent
             }
-        });
+        })
 
-        // await page.click("#txtRuc");
-        // await page.fill("#txtRuc", numRuc);
-        // await page.click("#btnAceptar");
-        // await page.waitForSelector(".list-group", { timeout: 60000, state: 'attached' });
+        // Aquí puedes agregar el código para extraer la información que necesitas de la página.
+        // Por ejemplo, podrías usar page.evaluate() para ejecutar código en el contexto de la página.
 
-        // const result = await page.evaluate(() => {
-        //     const elemento = document.querySelectorAll('.list-group')[0].children;
-         
-        //     const numeroRucName =  elemento[0].children[0].children[1].children[0].innerText.trim(); // el primer corchete decide cual salir
-        //     const tipoContribuyente =  elemento[1].children[0].children[1].children[0].innerText.trim();
-        //     const nombreComercial = elemento[2].children[0].children[1].children[0].innerHTML.trim(); 
-        //     const fechaInscripcion = elemento[3].children[0].children[1].children[0].innerText.trim();
-        //     const estadoContribuyente = elemento[4].children[0].children[1].children[0].innerText.trim(); 
-        //     const condicionContribuyente = elemento[5].children[0].children[1].children[0].innerText.trim(); 
-        //     const domicilioFiscal = elemento[6].children[0].children[1].children[0].innerText.trim();
-        //     const sistemaEmisionComprobante = elemento[7].children[0].children[1].children[0].innerText.trim();
-        //     const actividadComercio = elemento[7].children[0].children[3].children[0].innerText.trim();
-        //     const sistemaContabilidad = elemento[8].children[0].children[1].children[0].innerText.trim();
-     
-        //     const emisorElectronicoDesde = elemento[12].children[0].children[1].children[0].innerText.trim();
-        //     const comprobanteElectronico = elemento[13].children[0].children[1].children[0].innerText.trim();
-        //     const afiliadoAlPleDesde = elemento[14].children[0].children[1].children[0].innerText.trim();
-           
-          
-        //     return {
-        //      numeroRucName: numeroRucName,
-        //      tipoContribuyente: tipoContribuyente,
-        //      nombreComercial: nombreComercial,
-        //      fechaInscripcion: fechaInscripcion,
-        //      estadoContribuyente: estadoContribuyente,
-        //      condicionContribuyente: condicionContribuyente,
-        //      domicilioFiscal: domicilioFiscal,
-        //      sistemaEmisionComprobante: sistemaEmisionComprobante,
-        //      actividadComercio: actividadComercio,
-        //      sistemaContabilidad:sistemaContabilidad,
-        //      emisorElectronicoDesde:emisorElectronicoDesde,
-        //      comprobanteElectronico:comprobanteElectronico,
-        //      afiliadoAlPleDesde: afiliadoAlPleDesde,
-          
-        //     }
-            
-        // })
         return result;
     } catch (error) {
         console.error(error);
@@ -88,3 +35,58 @@ async function openWebPage(numRuc) {
 }
 
 export { openWebPage };
+
+
+
+
+            // await page.goto('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp', { timeout: 60000 });
+
+            // const result = await page.evaluate(()=>{
+            //     const el = document.querySelector("h1");
+            //     return {
+            //         [el.tagName]: el.textContent
+            //     }
+            // });
+    
+            // await page.click("#txtRuc");
+            // await page.fill("#txtRuc", numRuc);
+            // await page.click("#btnAceptar");
+            // await page.waitForSelector(".list-group", { timeout: 60000, state: 'attached' });
+    
+            // const result = await page.evaluate(() => {
+            //     const elemento = document.querySelectorAll('.list-group')[0].children;
+             
+            //     const numeroRucName =  elemento[0].children[0].children[1].children[0].innerText.trim(); // el primer corchete decide cual salir
+            //     const tipoContribuyente =  elemento[1].children[0].children[1].children[0].innerText.trim();
+            //     const nombreComercial = elemento[2].children[0].children[1].children[0].innerHTML.trim(); 
+            //     const fechaInscripcion = elemento[3].children[0].children[1].children[0].innerText.trim();
+            //     const estadoContribuyente = elemento[4].children[0].children[1].children[0].innerText.trim(); 
+            //     const condicionContribuyente = elemento[5].children[0].children[1].children[0].innerText.trim(); 
+            //     const domicilioFiscal = elemento[6].children[0].children[1].children[0].innerText.trim();
+            //     const sistemaEmisionComprobante = elemento[7].children[0].children[1].children[0].innerText.trim();
+            //     const actividadComercio = elemento[7].children[0].children[3].children[0].innerText.trim();
+            //     const sistemaContabilidad = elemento[8].children[0].children[1].children[0].innerText.trim();
+         
+            //     const emisorElectronicoDesde = elemento[12].children[0].children[1].children[0].innerText.trim();
+            //     const comprobanteElectronico = elemento[13].children[0].children[1].children[0].innerText.trim();
+            //     const afiliadoAlPleDesde = elemento[14].children[0].children[1].children[0].innerText.trim();
+               
+              
+            //     return {
+            //      numeroRucName: numeroRucName,
+            //      tipoContribuyente: tipoContribuyente,
+            //      nombreComercial: nombreComercial,
+            //      fechaInscripcion: fechaInscripcion,
+            //      estadoContribuyente: estadoContribuyente,
+            //      condicionContribuyente: condicionContribuyente,
+            //      domicilioFiscal: domicilioFiscal,
+            //      sistemaEmisionComprobante: sistemaEmisionComprobante,
+            //      actividadComercio: actividadComercio,
+            //      sistemaContabilidad:sistemaContabilidad,
+            //      emisorElectronicoDesde:emisorElectronicoDesde,
+            //      comprobanteElectronico:comprobanteElectronico,
+            //      afiliadoAlPleDesde: afiliadoAlPleDesde,
+              
+            //     }
+                
+            // })
