a58eadad1884b22a361e0bfdcd3bdde54ab6e702
RafaelCTobar <rafaelctobar569@gmail.com>
2024-04-20 21:43:24 -0500

    first try

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { openWebPage } from './miarchivo.js';
+import readInfo from './testDelete.js';
 
 const app = express();
+app.use(express.json())
 const port = process.env.PORT||3001;
 
 app.get('/scrape/:numRuc', async (req, res) => {
@@ -18,6 +20,13 @@ app.get('/scrape/:numRuc', async (req, res) => {
         console.error("Error al consultar RUC:", error);
         res.status(500).send('Ocurrió un error al consultar el RUC.');
     }
+});
+
+app.post("/test", (req, res)=>{
+    const data = req.body;
+    console.log(data)
+    readInfo(data);
+    res.json(data);
 });
 
 app.get("/check", (_, res)=>{
---
+++ testDelete.js
@@ -0,0 +1,5 @@
+export default function readInfo(info){
+    for(const key in info){
+        console.log(key);
+    }
+}
